bfbda3aa02186faf68f02f5619925318ac836fa6
razan-2 <razvan.andronachi2007@gmail.com>
2024-09-17 17:34:05 +0300

    some more components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import { useRoutes } from 'react-router-dom';
+import { useRoutes, Outlet } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import { NotFound } from './components/NotFound/NotFound';
 import { About } from './pages/About/About';
+import { NavBar } from './components/NavBar/NavBar';
+import { Footer } from './components/Footer/Footer';
+
+const Layout = () => {
+  return (
+    <>
+      <NavBar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  )
+}
 
 function App() {
   const element = useRoutes([
     {
       path: '/',
-      element: <Home />
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/about-us',
+          element: <About />
+        },
+        {
+          path: '*',
+          element: <NotFound />
+        }
+      ]
     },
-    {
-      path: '/about-us',
-      element: <About />
-    },
-    {
-      path: '*',
-      element: <NotFound />
-    }
-  ])
+  ]);
 
   return element;
 }
---
+++ client/src/pages/Home/Donate/Donate.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Donate = () => {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
