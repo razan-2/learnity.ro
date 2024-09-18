f7a88ae817c2765b4c0ee16d8abfa00dcd86b155
razan-2 <razvan.andronachi2007@gmail.com>
2024-09-18 19:39:14 +0300

    navbar fixes and sum other shit

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { FrequentQuestionsContextProvider } from '../../context/FrequentQuestions/FrequentQuestionsContext';
 import { FrequentQuestions } from './FrequentQuestions/FrequentQuestions';
+import { Donate } from './Donate/Donate';
+import { Header } from './Header/Header';
 
 export const Home = () => {
     return ( 
         <main className='bg-customWhite min-h-screen flex flex-col'>
+            <Header />
             <FrequentQuestionsContextProvider>
                 <FrequentQuestions />
             </FrequentQuestionsContextProvider>
+            <Donate />
         </main>
     );
 }
