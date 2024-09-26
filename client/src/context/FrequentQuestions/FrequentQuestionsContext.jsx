a4a1900ba69e49980d92607617fb0ed109e697b6
razan-2 <razvan.andronachi2007@gmail.com>
2024-09-26 16:19:26 +0300

    FrequentQuestions reloaded

--- client/src/context/FrequentQuestions/FrequentQuestionsContext.jsx
+++ client/src/context/FrequentQuestions/FrequentQuestionsContext.jsx
@@ -8,36 +8,42 @@ export const FrequentQuestionsContextProvider = (props) => {
             question: 'Lorem ipsum dolor sit amet, consectetur adipiscing?',
             answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
             about: 'Playground',
+            path: '/playground',
             id: 1
         },
         {
             question: 'Lorem ipsum odor amet, consectetuer adipiscing elit?',
             answer: 'Lorem ipsum odor amet, consectetuer adipiscing elit. Himenaeos sagittis neque eros primis vivamus cubilia lacus commodo. Lobortis aenean interdum torquent sapien urna quisque! ',
             about: 'Guided',
+            path: '/guided-learning',
             id: 2
         },
         {
             question: 'Lorem ipsum odor amet, consectetuer adipiscing elit?',
             answer: 'Lorem ipsum odor amet, consectetuer adipiscing elit. Vestibulum lectus id dis maecenas id finibus lorem. Ut fringilla turpis lacus suspendisse placerat habitasse. ',
             about: 'Donation',
+            path: '/doneaza',
             id: 3
         },
         {
             question: 'Lorem ipsum odor amet, consectetuer adipiscing elit?',
             answer: 'Lorem ipsum odor amet, consectetuer adipiscing elit. Vestibulum lectus id dis maecenas id finibus lorem. Ut fringilla turpis lacus suspendisse placerat habitasse. ',
             about: 'Donation',
+            path: '/doneaza',
             id: 4
         },
         {
             question: 'Lorem ipsum odor amet, consectetuer adipiscing elit?',
             answer: 'Lorem ipsum odor amet, consectetuer adipiscing elit. Vestibulum lectus id dis maecenas id finibus lorem. Ut fringilla turpis lacus suspendisse placerat habitasse. ',
             about: 'Donation',
+            path: '/doneaza',
             id: 5
         },
         {
             question: 'Lorem ipsum odor amet, consectetuer adipiscing elit?',
             answer: 'Lorem ipsum odor amet, consectetuer adipiscing elit. Vestibulum lectus id dis maecenas id finibus lorem. Ut fringilla turpis lacus suspendisse placerat habitasse. ',
             about: 'Donation',
+            path: '/doneaza',
             id: 6
         }
     ]);
